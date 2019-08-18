3c4564fd3c34cc662e9328122cf987d3a9671077
Juan Pablo Urango Vitola <jurangov@amazingsoulgames.com.co>
2019-08-18 17:18:28 -0500

    Adding mongo to docker compose

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 'use strict';
 
 const express = require('express');
+const { mongoose } = require('./database');
 
 // Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-// App
+//App
 const server = express();
+
+//Middleware
+server.use(express.json());
 
 //Settings
 server.set('port', process.env.PORT || PORT)
