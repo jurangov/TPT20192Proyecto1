55cd281b5284068ce9832c74b9b4f27ef2ee2533
Juan Pablo Urango Vitola <jurangov@amazingsoulgames.com.co>
2019-08-18 11:02:27 -0500

    Adding some methods to configure REST routes

--- server/index.js
+++ server/index.js
@@ -8,9 +8,15 @@ const HOST = '0.0.0.0';
 
 // App
 const server = express();
+
+//Settings
+server.set('port', process.env.PORT || PORT)
+
+//Routes
 server.get('/', (req, res) => {
   res.send('Hello world\n');
 });
 
-server.listen(PORT, HOST);
+//Starting server
+server.listen(server.get('port'), HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
